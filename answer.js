8b93efee09bfd8f4b5c1ae0fbd8a97f4878b8129
Ryugo <85220976+tanakaryugo@users.noreply.github.com>
2023-11-16 20:33:22 +0900

    test

--- answer.js
+++ answer.js
@@ -14,26 +14,28 @@ function onClick()
 {
     qst = document.getElementById('question').value;
     document.querySelector('.balloon1-top').innerHTML = qst;
+    
 }
 
 //qst = document.getElementById('question').value;
 
-var random = Math.random();
+// var random = Math.random();
 
-document.write("<h1>javascriptから喋ってます</h1>");
-var a = 2;
-var b = square(a);
+// document.write("<h1>javascriptから喋ってます</h1>");
+// var a = 2;
+// var b = square(a);
 
-document.write("<h1>");
-if (random <= 0.5)
-{
-    document.write(a + ", ");
-    document.write(random);
-}
-else
-{
-    document.write(b + ", ");
-    document.write(random);
-}
-document.write(qst);
-document.write("</h1>");
+// document.write("<h1>");
+// if (random <= 0.5)
+// {
+//     document.write(a + ", ");
+//     document.write(random);
+// }
+// else
+// {
+//     document.write(b + ", ");
+//     document.write(random);
+// }
+// document.write(qst);
+// document.write("</h1>");
+document.write("<p>" + qst + "</p>");
